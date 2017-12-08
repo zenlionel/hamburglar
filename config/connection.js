40eb0f466eea38088b1b591477b2f0c300b2e42b
Leo <zenlionel@gmail.com>
2017-12-08 13:45:34 -0800

    not working homework

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     port: 3306,
     user: 'root',
-    password: '3Nkerman',
+    password: '',
     database: 'burgers_db'
 });
 
