c4d89585a9d86cf1615ad528529af1df2d7b6ed2
Leo <zenlionel@gmail.com>
2018-02-15 19:00:45 -0800

    changed html layout for handlebars

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-// override with POST having ?_method=DELETE
+
 app.use(methodOverride("_method"));
 var exphbs = require("express-handlebars");
 
