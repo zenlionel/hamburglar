2d460bd443930c68beff8381ae702be72b8b62c0
Leo <zenlionel@gmail.com>
2018-02-15 19:06:14 -0800

    added orm for burger model

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,24 @@
-var orm = require('../config/orm.js');
+var orm = require("../config/orm.js");
 
 var burger = {
-    all: function (cb) {
-        orm.all("burgers", function (res) {
-            cb(res);
-        });
-    },
-    
-    create: function (cols, vals, cb) {
-        orm.create("burgers", cols, vals, function (res) {
-            cb(res);
-        });
-    },
-
-    update: function (objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
-    }
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devoured"
+    ], [
+      name, false
+    ], cb);
+  },
+  update: function(id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {
+      devoured: true
+    }, condition, cb);
+  }
 };
 
 module.exports = burger;
